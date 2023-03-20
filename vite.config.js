f3206224edb32734aef1701f5098159d3ef49a36
Luis Olivares <laog.sonic4@gmail.com>
2023-03-20 10:49:56 -0600

    Trying to upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/SchoolManager/"
+  plugins: [react()]
+//  base: "/SchoolManager/"
 })
