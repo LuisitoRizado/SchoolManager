e16b85d5b9c2cd4c43274a219cf5b36426d296b0
Luis Olivares <laog.sonic4@gmail.com>
2023-03-20 14:45:33 -0600

    Trying to upload

--- src/Docentes/pages/ModificarMateriaPage.jsx
+++ src/Docentes/pages/ModificarMateriaPage.jsx
@@ -14,7 +14,19 @@ export const ModificarMateriaPage = () => {
   const [profesor, setProfesor] = useState();
   const [hora, setHora] = useState();
   const [aula, setAula] = useState();
-  let inputProfesor, inputHora, inputAula;
+  const [carrera, setCarrera] = useState()
+  const [nombre, setNombre] = useState()
+  const [creditos, setCreditos] = useState()
+  const [cupo, setCupo] = useState()
+  const [semestre, setSemestre] = useState()
+  //peticiones
+  const [aulas, setAulas] = useState([]);
+  const [carreras, setCarreras] = useState([]);
+  const [horarios, setHorarios] = useState([]);
+
+
+  
+  let inputProfesor, inputHora, inputAula, inputId, inputCarrera,inputMateria,inputCreditos, inputCupo, inputSemestre;
   //FUNCIONES PARA LA FUNCIONALIDAD DE LA PAGINA ESTAS PUEDEN SER IMPORTADAS O NO
   const fetchUser = async () => {
     const URL = "https://rest-api-production-a5bf.up.railway.app/getMateria/" + id;
@@ -28,6 +40,24 @@ export const ModificarMateriaPage = () => {
         setMateria(data.slice(0,1));
       })
       .catch((err) => console.log(err));
+
+      //peticiones
+      const url2 = "https://rest-api-production-a5bf.up.railway.app/getAllHorarios";
+      fetch(url2)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setHorarios(data);
+        });
+      //pedir todas las maulas
+      fetch("https://rest-api-production-a5bf.up.railway.app/getAllAulas")
+        .then((res) => res.json())
+        .then((data) => setAulas(data));
+  
+      //traer todas las carreas
+      fetch("https://rest-api-production-a5bf.up.railway.app/getAllCarreras")
+        .then((res) => res.json())
+        .then((data) => setCarreras(data));
   };
 
   //Funciones para los datos que modifique
@@ -40,6 +70,27 @@ export const ModificarMateriaPage = () => {
   const onHandleAula = (e) => {
     setAula(e.target.value);
   };
+  const onHandleCreditos = (e) => {
+    setCreditos(e.target.value);
+  };
+  //-----handlers
+
+  const onHandleCupo = (e) => {
+    setCupo(e.target.value);
+  };
+  const onHandleSemestre = (e) => {
+    setSemestre(e.target.value);
+  };
+  const onHandleCarrera = (e) => {
+    setId_Carrera(e.target.value);
+  };
+
+  const onHandleHorario = (e) => {
+    setHorario(e.target.value);
+  };
+
+
+
   //Funciones para los botones
   const guardarCambios = () => {
     //Vamos a actualizar la materia con los datos que se cambiaron
@@ -84,6 +135,77 @@ export const ModificarMateriaPage = () => {
               className="form-control disabled"
               disabled={true}
             />
+             <label htmlFor="carrera" className="form-label">
+          Carrera
+        </label>
+        <select
+          className={"form-select mySelect "}
+          name='carrera'
+          id="id_carreras"
+          aria-label="Default select example"
+          onChange={(e) => {
+            onHandleCarrera(e);
+          }}
+        >
+
+        {carreras.length >= 1 ? (
+          carreras.map((carrera, index) => (
+            <option value={carrera.Id_Carrera} className={"opcion-" + index} key={index}>
+              {carrera.Nombre}
+            </option>
+          ))
+        ) : (
+          <option >No existen Carreras</option>
+        )}
+      </select>
+      <label htmlFor="horario" className="form-label">
+          Id horario
+        </label>
+        <select
+          className={"form-select mySelect "}
+          id="id_horario"
+          aria-label="Default select example"
+          onChange={(e) => {
+            onHandleHorario(e);
+          }}
+        >
+            <option value=""> </option>
+
+          {horarios.length >= 1 ? (
+            horarios.map((horario, index) => (
+              <option value={horario.Id_Horario} className={"opcion-" + index} key={index}>
+                {horario.Hora_Inicio_Lunes + " - " + horario.Hora_Final_Lunes}
+              </option>
+            ))
+          ) : (
+            <option >No existen horarios</option>
+          )}
+        </select>
+
+        <label htmlFor="aula" className="form-label">
+          Id Aula
+        </label>
+        <select
+          className={"form-select mySelect "}
+          id="id_aula"
+          aria-label="Default select example"
+          onChange={(e) => {
+            onHandleAula(e);
+          }}
+        >
+            <option value=""> </option>
+
+          {aulas.length >= 1 ? (
+            aulas.map((aula, index) => (
+              <option value={aula.Id_Aula} className={"opcion-" + index} key={index}>
+                {aula.Nombre}
+              </option>
+            ))
+          ) : (
+            <option >No existen horarios</option>
+          )}
+        </select>
+
             {/*Nombre de la materia. */}
             <label htmlFor="" className="form-label">
               Nombre materia
@@ -97,46 +219,42 @@ export const ModificarMateriaPage = () => {
               defaultValue={mat.Materia}
             />
             {/*Profesor que imparte la materia */}
-            <label htmlFor="" className="form-label">
-              Profesor
-            </label>
-            <input
-              type="text"
-              name="profesor"
-              id="profesor"
-              className="form-control"
-              defaultValue={mat.Nombre + " " + mat.Ap_Paterno + " " + mat.Ap_Materno}
-
-              onChange={(e)=>onHandleProfesor(e)}
-            />
+            <label htmlFor="creditos" className="form-label">
+          Creditos
+        </label>
+        <input
+          type="number"
+          name="creditos"
+          id="creditos"
+          onKeyPress={validarNumeros}
+          className="form-control"
+          onChange={(e) => onHandleCreditos(e)}
+        />
             {/*Hora de la materia */}
-            <label htmlFor="" className="form-label">
-              Hora
-            </label>
-            <input
-              type="text"
-              name="hora"
-              id="hora"
-              className="form-control"
-              
-              defaultValue={mat.Hora_Inicio_Lunes}
-              onChange={(e)=>onHandleHora(e)}
-
-            />
+           
             {/*Aula donde se imparte la materia */}
-            <label htmlFor="" className="form-label">
-              Aula
-            </label>
-            <input
-              type="text"
-              name="aula"
-              id="aula"
-              className="form-control"
-            
-              defaultValue={mat.Hora_Final_Lunes}
-              onChange={(e)=>onHandleAula(e)}
-
-            />
+            <label htmlFor="cupo" className="form-label">
+          Cupo
+        </label>
+        <input
+          type="number"
+          name="cupo"
+          id="cupo"
+          onKeyPress={validarNumeros}
+          className="form-control"
+          onChange={(e) => onHandleCupo(e)}
+        />
+        <label htmlFor="semestre" className="form-label">
+          Semestre
+        </label>
+        <input
+          type="number"
+          onKeyPress={validarNumeros}
+          name="semestre"
+          id="semestre"
+          className="form-control"
+          onChange={(e) => onHandleSemestre(e)}
+        />
             <button
               className="btn btn-danger m-2"
               onClick={() => guardarCambios()}
