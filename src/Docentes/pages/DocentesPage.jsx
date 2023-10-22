23c797944c24eda1e137f622dd0d7fcf8c629097
LuisitoRizado <laog.sonic4@gmail.com>
2023-10-22 10:34:27 -0600

    Ultimo testeo

--- src/Docentes/pages/DocentesPage.jsx
+++ src/Docentes/pages/DocentesPage.jsx
@@ -17,7 +17,7 @@ export const DocentesPage = () => {
               </div>
               <div className="col-md-8">
                 <div className="card-body">
-                  <h5 className="card-title">Agregar Docente</h5>
+                  <h5 className="card-title">Agregar Docentessssssssss</h5>
                   <p className="card-text">
                    Puedes agregar docentes disponibles para las materias
                   </p>
