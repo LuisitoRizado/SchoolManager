f3f3a16f250f8c8da37a53765235dfd8c8c220c0
LuisitoRizado <laog.sonic4@gmail.com>
2023-11-26 01:02:14 -0600

    adding

--- src/Docentes/pages/DocentesPage.jsx
+++ src/Docentes/pages/DocentesPage.jsx
@@ -89,18 +89,29 @@ export const DocentesPage = () => {
               </NavLink>
                 </div>
               </div>
+            </div>
+          </div>
 
+          <div className="card m-3 col-md-5 shadow">
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/1055/1055644.png"
+                  className="img-fluid rounded-start"
+                  alt="..."
+                />
+              </div>
               <div className="col-md-8">
                 <div className="card-body">
                   <h5 className="card-title">Calificaciones</h5>
                   <p className="card-text">
-                  Asigna calificaciones a alumnos
+                  Asigna calificaciones
                   </p>
                   <br />
                   <NavLink
                 className={'btn btn-primary'}
                 to={{
-                  pathname: 'asignarCalificaciones'
+                  pathname: 'calificacionesAlumnos'
                 }}
               >
                 Acceder
