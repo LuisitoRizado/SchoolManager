9e889c9ad8360b137f994ccf056ac99d1f1b604a
LuisitoRizado <laog.sonic4@gmail.com>
2023-11-26 01:44:39 -0600

    adding

--- src/auth/pages/ProfesorRoutes.jsx
+++ src/auth/pages/ProfesorRoutes.jsx
@@ -1,6 +1,9 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { NavbarProfesor } from '../../ui/components/NavbarProfesor';
 import { InicioPage } from '../../Profesores/InicioPage';
+import { CalificacionesPage } from '../../Profesores/CalificacionesPage';
+import { ReporteDocentePage } from '../../Profesores/ReporteDocentePage';
+
 
 
 export const ProfesorRoutes = () => {
@@ -11,9 +14,9 @@ export const ProfesorRoutes = () => {
         <div className="container-fluid">
             <Routes>
                 <Route path="inicio/*" element={<InicioPage />} />
-                <Route path="reportes/" element={<HorarioPDF />} />
-                <Route path="calificaciones/" element={<HorarioPDF />} />
-                <Route path="misDatos/" element={<HorarioPDF />} />
+                <Route path="reportes/" element={<ReporteDocentePage />} />
+                <Route path="calificaciones/" element={<CalificacionesPage />} />
+                <Route path="misDatos/" element={<ReporteDocentePage />} />
 
                
 
